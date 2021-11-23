6dd0b07c96ff933e34f76e07e1142c9736680c55
roberto montalti <roberto.montalti00@gmail.com>
2021-11-23 21:10:01 +0100

    added gui consts and messages consts

--- src/consts.rs
+++ src/consts.rs
@@ -6,8 +6,16 @@ pub const LIMIT_FPS: i32 = 60;
 pub const SCREEN_WIDTH: i32 = 80;
 pub const SCREEN_HEIGHT: i32 = 50;
 
+pub const BAR_WIDTH: i32 = 20;
+pub const PANEL_HEIGHT: i32 = 7;
+pub const PANEL_Y: i32 = SCREEN_HEIGHT - PANEL_HEIGHT;
+
+pub const MSG_X: i32 = BAR_WIDTH + 2;
+pub const MSG_WIDTH: i32 = SCREEN_WIDTH - BAR_WIDTH - 2;
+pub const MSG_HEIGHT: usize = PANEL_HEIGHT as usize - 1;
+
 pub const MAP_WIDTH: i32 = 80;
-pub const MAP_HEIGHT: i32 = 45;
+pub const MAP_HEIGHT: i32 = 43;
 
 pub const ROOM_MAX_SIZE: i32 = 10;
 pub const ROOM_MIN_SIZE: i32 = 6;
